ac2032aba5a55991192e2558d311df1ff2a8373d
faissahrul-alt <namadepanfais@gmail.com>
2025-11-15 20:45:10 +0700

    Add files via upload

---
+++ bangunhitung.py
@@ -0,0 +1,8 @@
+def luas_persegipanjang(panjang,lebar):
+    luas = panjang * lebar
+    return luas
+
+p = float(input("Masukkan Nilai :"))
+l = float(input("Masukkan Nilai :"))
+
+print("Panjang persegi adalah  :",luas_persegipanjang(p,l))
